6488c13b5af1ca3e35b6d045ed170d2c50fdca8d
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-11-03 21:47:49 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let interval = null;
+
+window.addEventListener('DOMContentLoaded', () => {
+  refs.stop.disabled = true;
+});
+
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick);
+
+function onStartClick(e) {
+  interval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  toggleDisabledButton(e.target, refs.stop);
+}
+
+function onStopClick(e) {
+  clearInterval(interval);
+  toggleDisabledButton(e.target, refs.start);
+}
+
+function toggleDisabledButton(el, sibling) {
+  el.disabled = el.hasAttribute('disabled') ? false : true;
+  sibling.disabled = sibling.hasAttribute('disabled') ? false : true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
