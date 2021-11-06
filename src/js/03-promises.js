ead0ec83be5db69e05494fd3ea76e7b9eec74ce2
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-11-06 20:21:11 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const firstDelay = +e.currentTarget.elements.delay.value;
+  let delay = firstDelay;
+
+  for (let i = 1; i <= e.currentTarget.elements.amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(result => {
+        Notiflix.Notify.success(result);
+      })
+      .catch(error => {
+        Notiflix.Notify.failure(error);
+      });
+    delay += +e.currentTarget.elements.step.value;
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
 }
